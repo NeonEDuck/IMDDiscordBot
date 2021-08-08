2122b2a0f7c21e512f784ebbd7380fb7fab4360d
NEDuck <cj96cj9696cj@gmail.com>
2021-08-09 02:09:08 +0800

    data資料夾偵測

--- init.py
+++ init.py
@@ -1,6 +1,9 @@
 import os
 
 def run():
+    if not os.path.isdir('data'):
+        os.mkdir('data')
+
     if not os.path.isfile('data/vote.json'):
         with open('data/vote.json', 'w', encoding='utf-8') as f:
             f.write('{}')
