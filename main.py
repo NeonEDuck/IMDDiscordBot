434115f9af5c840d6e84d5cefb3262028dec3db8
NEDuck <cj96cj9696cj@gmail.com>
2021-08-15 13:18:17 +0800

    增加Misc

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from threading import Thread
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand
-from discord_slash.context import SlashContext
 from variable import TOKEN
 import error_handler
 import app
@@ -11,11 +10,7 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(intents=intents, command_prefix='nebot:')
 slash = SlashCommand(bot, sync_commands=True)
-extensions = ['cogs.vote', 'cogs.permission']
-
-@slash.slash(name='ping')
-async def _ping(ctx:SlashContext):
-    await ctx.send(f'Pong! ({bot.latency*1000}ms)', hidden=True)
+extensions = ['cogs.vote', 'cogs.permission', 'cogs.misc']
 
 if __name__ == '__main__':
     error_handler.setup(bot)
